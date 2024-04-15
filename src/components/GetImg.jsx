b18cdf12d17203099350f656a4e000cf7e762d81
Karfalla <diabykarfalla2@gmail.com>
2024-04-15 13:10:47 +0000

    Ajout d'une animation de loading

--- src/components/GetImg.jsx
+++ src/components/GetImg.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 
 export default function GetImg() {
     const [images, setImages] = useState([])
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchImages = async () => {
@@ -17,12 +18,22 @@ export default function GetImg() {
         fetchImages()
     }, [])
 
+    useEffect(() => {
+      // Simuler un chargement asynchrone
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+  
+      return () => clearTimeout(timer);
+    }, []);
+
   return (
     <>  
-        {!images ? (
-          <div>
-            <h1>Loading...</h1>
+        {loading ? (
+          <div className='w-full flex justify-center'>
+            <div class="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin absolute"></div>
           </div>
+          
         ) : (
             <ul className="[column-count:4] gap-x-[20px][@media(max-width:1200px)]: [@media(max-width:1200px)]:[column-count:3] [@media(max-width:1200px)]:gap-x-[20px] [@media(max-width:800px)]: [@media(max-width:800px)]:[column-count:2] [@media(max-width:800px)]:gap-x-[20px] [@media(max-width:600px)]: [@media(max-width:600px)]:[column-count:1]">
                 <li className="">
